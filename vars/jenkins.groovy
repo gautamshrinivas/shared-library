ee4fa39da36d6136e6c986c536939c01ba5da7f8
gautam <gautamrck76@gmail.com>
2022-10-23 11:40:37 +0530

    added

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,13 +1,17 @@
-def call {
+def call() {
     pipeline {
         agent { label 'node'} 
         stages {
             stage ('vcs') {
+                steps {
                 git url:'https://github.com/gautamshrinivas/spring-petclinic.git',
                 branch:'oct21'
+                }
             }
             stage ('build') {
+                steps {
                 sh 'mvn package'
+                }                
             }
         }
     }
